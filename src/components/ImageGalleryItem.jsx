3bd719ff1ea37ddd2b7d732f7d514ffd79ec6162
Dariaaal <1406dariaal@gmail.com>
2023-03-19 01:03:35 +0200

    prop types

--- src/components/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem.jsx
@@ -1,9 +1,17 @@
-import css from './ImageGalleryItem.module.css'
+import css from './ImageGalleryItem.module.css';
+import PropTypes from 'prop-types';
 
 const ImageGalleryItem = ({id, webformatURL, largeImageURL, tags}) => {
     return <li key={id} className={css.ImageGalleryItem}>
     <a href={largeImageURL}><img src={webformatURL} alt={tags} className={css.image}/></a>
   </li>
+}
+
+ImageGalleryItem.propTypes = {
+  id: PropTypes.number,
+  webformatURL: PropTypes.string,
+  largeImageURL: PropTypes.string,
+  tags: PropTypes.string,
 }
 
 export default ImageGalleryItem;
