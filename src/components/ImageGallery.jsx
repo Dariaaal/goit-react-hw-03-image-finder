756faf919e2507008484226d90166dfdbb15aa9b
Dariaaal <1406dariaal@gmail.com>
2023-03-19 19:08:20 +0200

    loader

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,21 +2,38 @@ import React, { Component } from "react";
 import getImages from './api';
 import ImageGalleryItem from "./ImageGalleryItem";
 import css from './ImageGallery.module.css';
+import {Loader} from './Loader';
 
 export default class ImageGallery extends Component {
   state = {
     items: null,
+    isLoading: false,
+    error: false,
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.searchText !== this.props.searchText) {
-      getImages(this.props.searchText).then((items) => this.setState({items: items.hits}))
+  async componentDidUpdate(prevProps, prevState) {
+      if (prevProps.searchText !== this.props.searchText) {
+    try {
+      this.setState({isLoading: true});
+      const items = await getImages(this.props.searchText)
+      this.setState({items: items.hits, isLoading: false})
+      }
+    catch (error) {
+      this.setState({error: true, isLoading: false});
+      console.log(error);
     }
+    finally {
+      this.setState({isLoading: false})
+    }
+  }
   }
 
   render() {
-    const {items} = this.state;
+    const {items, isLoading, error} = this.state;
     return (
+      <div>
+        {error && (<p>Nothing was found</p>)}
+        {isLoading ? <Loader/> : 
         <ul className={css.ImageGallery}>
             {items && items.map((item)=>(
                 <ImageGalleryItem
@@ -26,7 +43,8 @@ export default class ImageGallery extends Component {
                 tags={item.tags}
                 />
             ))}
-        </ul>
+        </ul>}
+      </div>
     )
   }
 }
